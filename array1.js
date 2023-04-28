eadef8fd4e5191e870871a2970736dd00629d547
punam123456789 <punamp073@gmail.com>
2023-04-28 21:59:52 +0530

    updated

--- array1.js
+++ array1.js
@@ -15,9 +15,34 @@
 // }
 
 
-var array = [34,65,567,78,34,67,87,56,89];
-var object={};
-for(i=0;i<array.length;i++){
-    object[i]=array[i];
+// var array = [34,65,567,78,34,67,87,56,89];
+// var object={};
+// for(i=0;i<array.length;i++){
+//     object[i]=array[i];
+// }
+// console.log(object);
+
+
+var arr = [1,2,3,4,5,6];
+var even = [];
+var odd = [];
+flag=false
+for(var i=0;i<arr.length;i++){
+    if(arr[i]%2==0) {
+        even.push(arr[i]);
+        flag=true;
+
+    }
+        // console.log(even)
 }
-console.log(object);
+   if(flag=true){
+    console.log(even)
+   }
+   else{
+    odd.push(arr[i]);
+
+ console.log(odd)
+  
+   }  
+  
+            
