fb0ff309391d1368b65967cf9bd237a4c7f56f49
punam123456789 <punamp073@gmail.com>
2023-05-02 17:50:07 +0530

    updated

--- test.js
+++ test.js
@@ -14,35 +14,64 @@
 // console.log("sum of n numbers for divisible by 3,5 or 7 is- " , sum);
 
 
+// ***********************************************************************************************
+// function getMaxLength(arr, n) {
 
-function getMaxLength(arr, n) {
-
-	var count = 0;
+// 	var count = 0;
 
 	
-	var result = 0;
+// 	var result = 0;
 
-	for (var i = 0; i < n; i++) {
+// 	for (var i = 0; i < n; i++) {
 
-		if (arr[i] == 0)
-			count = 0;
+// 		if (arr[i] == 0)
+// 			count = 0;
 
 		
-		else {
+// 		else {
 			
-			count++;
-			result = Math.max(result, count);
-		}
-	}
+// 			count++;
+// 			result = Math.max(result, count);
+// 		}
+// 	}
 
-	return result;
-}
-
-
-var arr = new Array(1, 1, 0, 0,
-	 1, 1);
-var n = arr.length;
-console.log(getMaxLength(arr, n));
+// 	return result;
+// }
 
 
+// var arr = new Array(1, 1, 0, 0, 1, 1);
+// var n = arr.length;
+// console.log(getMaxLength(arr, n));
 
+
+// *******************************************************************************************
+// var trunc=(s,k)=>{
+// 	var count = 0;
+// 	for(var i=0;i<s.length;i++){
+// 		if(s[i]===' ') 
+// 		{
+// 			count++;
+// 		}
+// 		if(count === k){
+// 			s=s.slice(0,i)
+// 		}
+// 	}
+// 	return s;
+
+// };
+// console.log(trunc('hello how are you all',4));
+
+//*************************************************************************
+// var array =[66,3,55,9,8];
+// var minvalue = array[0]; 
+// for (var i = 0; i < array.length; i++) {
+//     if(array[i]<minvalue)
+//     {
+//         minvalue = array[i];
+//     }
+
+// }
+//   console.log(minvalue);
+
+
+// *******************************************************************************
